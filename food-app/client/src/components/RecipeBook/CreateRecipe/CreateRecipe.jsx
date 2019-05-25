310ab8d3fb4e5ba43ae1a5fc9874c3474415b366
Leticia Zamolo Duque <lzduque@hotmail.com>
2019-05-25 12:00:31 -0400

    set status for each change in the form

--- food-app/client/src/components/RecipeBook/CreateRecipe/CreateRecipe.jsx
+++ food-app/client/src/components/RecipeBook/CreateRecipe/CreateRecipe.jsx
@@ -1,6 +1,51 @@
 import React, { Component } from 'react';
 
 class CreateRecipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { recipeTitle: "",
+                    mealType: "meal",
+                    preparation: "",
+                    recipeImg: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    // check it out: we get the evt.target.name (which will be either "email" or "password")
+    // and use it to target the key on our `state` object with the same name, using bracket syntax
+    this.setState({ [event.target.name]: event.target.value });
+    console.log('this.state.recipeTitle: ', this.state.recipeTitle)
+    console.log('this.state.mealType: ', this.state.mealType)
+    console.log('this.state.preparation: ', this.state.preparation)
+    console.log('this.state.recipeImg: ', this.state.recipeImg)
+  }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   alert('A recipe was created: ' + this.state.recipeTitle);
+
+  //   const search = ([recipeTitle, mealType], callback) => {
+  //     return fetch(`/api/recipe/create?recipeTitle=${recipeTitle}?mealType=${mealType}`, {
+  //         accept: 'application/json',
+  //       }).then(checkStatus)
+  //       .then( (response) => {
+  //         return response.json();
+  //       })
+  //       .then(callback)
+
+  //     search(this.state.name, (user) => {
+  //       console.log(user);
+  //       this.setState({
+  //         user
+  //       });
+  //     });
+
+  //   };
+  // }
+
+
   render() {
     return (
       <div className="create-recipe container-1">
@@ -11,7 +56,10 @@ class CreateRecipe extends Component {
         <form>
           <label>
             <div className="container-1-box">
-              <h2>Recipe Title - input with placeholder</h2>
+              <input name="recipeTitle"
+                      type="text"
+                      value= {this.state.recipeTitle}
+                      onChange={this.handleChange} />
             </div>
           </label>
           <label>
@@ -25,7 +73,7 @@ class CreateRecipe extends Component {
           <label>
             <div className="container-1-box">
               <h3>Recipe Image</h3>
-              <p>upload image from computer</p>
+              <input name="recipeImg" type="file" />
             </div>
           </label>
           <label>
@@ -43,22 +91,22 @@ class CreateRecipe extends Component {
           <label>
             <div className="container-1-box">
               <h3>Meal Type (one choice only)</h3>
-              <p>checklist Breakfast</p>
-              <p>checklist Meal</p>
-              <p>checklist Snack</p>
+              <select name="mealType" value={this.state.mealType} onChange={this.handleChange}>
+                <option value="breakfast">Breakfast</option>
+                <option value="meal">Meal</option>
+                <option value="snack">Snack</option>
+              </select>
             </div>
           </label>
           <label>
             <div className="container-1-box">
               <h3>Preparation</h3>
-              <p>free textarea that will take XX max of characters</p>
+              <textarea name="preparation" value={this.state.preparation} onChange={this.handleChange} />
             </div>
           </label>
-          <label>
-            <div className="container-1-box">
-              <p>Create Recipe Button</p>
-            </div>
-          </label>
+          <div className="container-1-box">
+            <input type="submit" value="Create Recipe" />
+          </div>
         </form>
       </div>
     )
