5aa1666133aab567b9025d04ec6f0c089f93a3a0
Leticia Zamolo Duque <lzduque@hotmail.com>
2019-05-25 11:02:26 -0400

    structuring html code for create-recipe with form

--- food-app/client/src/components/RecipeBook/CreateRecipe/CreateRecipe.jsx
+++ food-app/client/src/components/RecipeBook/CreateRecipe/CreateRecipe.jsx
@@ -8,40 +8,58 @@ class CreateRecipe extends Component {
           <h1>Create Recipe Page</h1>
         </div>
         <hr />
-        <div className="container-1-box">
-          <h2>Recipe Title - input with placeholder</h2>
-        </div>
-        <div className="container-1-box">
-          <h3>Ingridients</h3>
-          <p>autosuggest ingridients here!</p>
-          <p>Ingridient name(autossugest - only ingridients that we have - error message) - qt.(input field that acepts only integers) - unit(hardcoded, will appear when the ingridient is choosen)</p>
-          <p>automatically another line like this appears to add another ingridient</p>
-        </div>
-        <div className="container-1-box">
-          <h3>Recipe Image</h3>
-          <p>upload image from computer</p>
-        </div>
-        <div className="container-1-box">
-          <h3>Servings per Recipe</h3>
-          <p>qt.(input field that acepts only integers)</p>
-        </div>
-        <div className="container-1-box">
-          <h3>Coocking Time</h3>
-          <p>qt.(input field that acepts only integers) minutes (hardcoded string)</p>
-        </div>
-        <div className="container-1-box">
-          <h3>Meal Type (one choice only)</h3>
-          <p>checklist Breakfast</p>
-          <p>checklist Meal</p>
-          <p>checklist Snack</p>
-        </div>
-        <div className="container-1-box">
-          <h3>Preparation</h3>
-          <p>free textarea that will take XX max of characters</p>
-        </div>
-        <div className="container-1-box">
-          <p>Create Recipe Button</p>
-        </div>
+        <form>
+          <label>
+            <div className="container-1-box">
+              <h2>Recipe Title - input with placeholder</h2>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <h3>Ingridients</h3>
+              <p>autosuggest ingridients here!</p>
+              <p>Ingridient name(autossugest - only ingridients that we have - error message) - qt.(input field that acepts only integers) - unit(hardcoded, will appear when the ingridient is choosen)</p>
+              <p>automatically another line like this appears to add another ingridient</p>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <h3>Recipe Image</h3>
+              <p>upload image from computer</p>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <h3>Servings per Recipe</h3>
+              <p>qt.(input field that acepts only integers)</p>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <h3>Coocking Time</h3>
+              <p>qt.(input field that acepts only integers) minutes (hardcoded string)</p>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <h3>Meal Type (one choice only)</h3>
+              <p>checklist Breakfast</p>
+              <p>checklist Meal</p>
+              <p>checklist Snack</p>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <h3>Preparation</h3>
+              <p>free textarea that will take XX max of characters</p>
+            </div>
+          </label>
+          <label>
+            <div className="container-1-box">
+              <p>Create Recipe Button</p>
+            </div>
+          </label>
+        </form>
       </div>
     )
   }
