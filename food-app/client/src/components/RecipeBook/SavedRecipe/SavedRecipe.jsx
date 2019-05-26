e70e81ab4e75f7282e17905de5996abab770ffa2
Sahanah Ganesh <sahanah@Sahanahs-MacBook-Pro.local>
2019-05-26 03:24:16 -0400

    fixes image carousel

--- food-app/client/src/components/RecipeBook/SavedRecipe/SavedRecipe.jsx
+++ food-app/client/src/components/RecipeBook/SavedRecipe/SavedRecipe.jsx
@@ -1,13 +1,98 @@
 import React, { Component } from 'react';
 
+import rec1 from './rec1.jpg';
+import rec2 from './rec2.jpg';
+import rec3 from './rec3.jpg';
+
+// console.log(rec1, rec2, rec3)
+
+const IMAGES = [rec1, rec2, rec3];
+const IMG_WIDTH = '300px';
+const IMG_HEIGHT = '300px';
+
+const RIGHT = '-1';
+const LEFT = '+1';
+const CENTER = '0';
+
+const buttonStyles = {
+  height: IMG_HEIGHT,
+  color: "#eeeeee",
+  fontSize: "2em",
+};
+
 class SavedRecipe extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { imageIdx: 0 };
+  }
+
+  onClick(direction) {
+    if (direction === CENTER) {
+      return alert("GO TO VIEW RECIPE")
+    }
+    const change = direction === RIGHT ? RIGHT : LEFT;
+    const adjustedIdx = this.state.imageIdx + Number(change);
+    let newIdx;
+    if (adjustedIdx >= IMAGES.length) {
+      newIdx = 0;
+    } else if (adjustedIdx < 0) {
+      newIdx = IMAGES.length - 1
+    } else {
+      newIdx = adjustedIdx;
+    }
+    this.setState({ imageIdx: newIdx });
+  }
+
   render() {
+    const { imageIdx = 0 } = this.state;
+    const imageStyles = {
+      order: 3,
+      flexDirection: 'column',
+      width: IMG_WIDTH,
+      height: IMG_HEIGHT,
+      backgroundImage: `url(${IMAGES[imageIdx]})`
+    };
+    const searchStyle = {
+      display: 'flex',
+      flexDirection: 'column'
+    };
     return (
-      <div>
-      <p>Saved Recipe Page</p>
+      <div style={searchStyle}>
+        <font align="center" size="3" color="green"><h1>Saved Recipes</h1></font>
+      <div className="carouselContainer">
+        <div style={imageStyles}>
+          <button
+            onClick={()=>this.onClick(RIGHT)}
+            className="hollow float-left"
+            style={buttonStyles}>⇦</button>
+          <button
+            onClick={()=>this.onClick(LEFT)}
+            className="hollow float-right"
+            style={buttonStyles}>⇨</button>
+          <div style={{
+            order: 4,
+            justifyContent:'center',
+            alignItems:'center'
+            }}>
+            <button
+              onClick={()=>this.onClick(CENTER)}
+              className="hollow center"
+              style={buttonStyles}>⇪</button>
+            </div>
+          </div>
+        </div>
+        <font align="center" size="1">Recipe: {imageIdx + 1}</font>
       </div>
     )
   }
 };
 
 export default SavedRecipe;
+
+
+
+
+
+
+
--- food-app/client/src/components/RecipeBook/SearchRecipe/SearchRecipe.jsx
+++ food-app/client/src/components/RecipeBook/SearchRecipe/SearchRecipe.jsx
@@ -1,98 +1,144 @@
 import React, { Component } from 'react';
 
-import rec1 from './rec1.jpg';
-import rec2 from './rec2.jpg';
-import rec3 from './rec3.jpg';
+const recipeView = () => {
+  alert("GO TO VIEW RECIPE")
+}
 
-// console.log(rec1, rec2, rec3)
+const Card = (props) => (
+  <div className="card" style={{
+    flex: '1 0 250px',
+    boxShadow: '#fff',
+    margin: '1 rem',
+    overflow: 'hidden',
+    borderRadius: '6 px',
+    cursor: 'pointer',
+    transition: 'all 250 ms ease - in -out'
+  }}>
+    <img src={ props.imgUrl } style={{
+      width: '250px',
+      height: '250px',
+      objectFit: 'center'
+    }}
+      alt={ props.alt || 'Image' } />
+    <div className="card-content" style={{
+      padding: '1rem'
+    }}>
+      <h2>{ props.title }</h2>
+      <p>{ props.content }</p>
+    </div>
+  </div>
+);
 
-const IMAGES = [rec1, rec2, rec3];
-const IMG_WIDTH = '300px';
-const IMG_HEIGHT = '300px';
+const CardContainer = (props) => (
 
-const RIGHT = '-1';
-const LEFT = '+1';
-const CENTER = '0';
-
-const buttonStyles = {
-  height: IMG_HEIGHT,
-  color: "#eeeeee",
-  fontSize: "2em",
-};
+  <div className="cards-container" style={{
+    display: 'flex',
+    borderRadius: '6 px',
+    color: 'grey',
+    background: 'white',
+    padding: '1rem',
+    boxShadow: '0 0 1 rem #000 inset',
+    overflow: 'auto',
+    position: 'relative'
+  }}>
+    {
+      props.cards.map((card) => (
+        <button key={ card.id} onClick={ recipeView }><Card
+          key={ card.id }
+          title={ card.title }
+          content={ card.content }
+          imgUrl={ card.imgUrl }
+          /></button>
+      ))
+    }
+  </div>
+);
 
 class SearchRecipe extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { imageIdx: 0 };
-  }
-
-  onClick(direction) {
-    if (direction === CENTER) {
-      return alert("GO TO VIEW RECIPE")
-    }
-    const change = direction === RIGHT ? RIGHT : LEFT;
-    const adjustedIdx = this.state.imageIdx + Number(change);
-    let newIdx;
-    if (adjustedIdx >= IMAGES.length) {
-      newIdx = 0;
-    } else if (adjustedIdx < 0) {
-      newIdx = IMAGES.length - 1
-    } else {
-      newIdx = adjustedIdx;
-    }
-    this.setState({ imageIdx: newIdx });
-  }
-
   render() {
-    const { imageIdx = 0 } = this.state;
-    const imageStyles = {
-      order: 3,
-      flexDirection: 'column',
-      width: IMG_WIDTH,
-      height: IMG_HEIGHT,
-      backgroundImage: `url(${IMAGES[imageIdx]})`
-    };
-    const searchStyle = {
-      display: 'flex',
-      flexDirection: 'column'
-    };
+
+    const cardsData = [
+    {
+      id: 1,
+      title: 'RECIPE 1',
+      content: 'Baked Buckwheat Pancakes',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_f6b265dfea8a45fb819eecb218e8603d~mv2_d_4000_6000_s_4_2.jpg/v1/fill/w_1260,h_1890,al_c,q_90,usm_0.66_1.00_0.01/26357d_f6b265dfea8a45fb819eecb218e8603d~mv2_d_4000_6000_s_4_2.webp'
+    },
+    {
+      id: 2,
+      title: 'RECIPE 2',
+      content: 'Bean & Oat Burger with 3-ingredient Cajun Mayo',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_a448d02540754527adf782f65a95b34c~mv2_d_2592_3888_s_4_2.jpg/v1/fill/w_1260,h_1890,al_c,q_90,usm_0.66_1.00_0.01/26357d_a448d02540754527adf782f65a95b34c~mv2_d_2592_3888_s_4_2.webp'
+    },
+    {
+      id: 3,
+      title: 'RECIPE 3',
+      content: 'Purple Burrito Bowl With Avocado Mango Salsa',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_1f83669e5fcd4c0884090855adad1c9a~mv2.jpg/v1/fill/w_683,h_1024,al_c,q_85/26357d_1f83669e5fcd4c0884090855adad1c9a~mv2.webp'
+    },
+    {
+      id: 4,
+      title: 'RECIPE 4',
+      content: 'Banana Coconut Lime Sorbet',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_c126eaddede64c2a8b4dc3b947cee15b~mv2_d_3456_5184_s_4_2.jpg/v1/fill/w_1260,h_1890,al_c,q_90,usm_0.66_1.00_0.01/26357d_c126eaddede64c2a8b4dc3b947cee15b~mv2_d_3456_5184_s_4_2.webp'
+    },
+    {
+      id: 5,
+      title: 'RECIPE 5',
+      content: 'Coconut Curry Cream of Squash Soup with Spiced Lentil Croutons',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_363c5eeb30b04f82bf9911571cb934c5~mv2_d_3888_2592_s_4_2.jpg/v1/fill/w_1260,h_840,al_c,q_85,usm_0.66_1.00_0.01/26357d_363c5eeb30b04f82bf9911571cb934c5~mv2_d_3888_2592_s_4_2.webp'
+    },
+    {
+      id: 6,
+      title: 'RECIPE 6',
+      content: 'Marinated Sesame Mushrooms',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_2703d1cc18d9458b9a25ec2302eb5f7a~mv2.jpg/v1/fill/w_1024,h_683,al_c,q_85/26357d_2703d1cc18d9458b9a25ec2302eb5f7a~mv2.webp'
+    },
+    {
+      id: 7,
+      title: 'RECIPE 7',
+      content: 'Chocolate Chia Breakfast Pudding',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_7195fe55859040cba59576d838a24ec8~mv2_d_5184_3456_s_4_2.jpg/v1/fill/w_1260,h_840,al_c,q_85,usm_0.66_1.00_0.01/26357d_7195fe55859040cba59576d838a24ec8~mv2_d_5184_3456_s_4_2.webp'
+    },
+    {
+      id: 8,
+      title: 'RECIPE 8',
+      content: 'Candied Coconut & Cinnamon Pecans',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_b1081d06969d455d80c78b45cd120431~mv2_d_6000_4000_s_4_2.jpg/v1/fill/w_1260,h_840,al_c,q_85,usm_0.66_1.00_0.01/26357d_b1081d06969d455d80c78b45cd120431~mv2_d_6000_4000_s_4_2.webp'
+    },
+    {
+      id: 9,
+      title: 'RECIPE 9',
+      content: 'Balsamic & Basil Bruschetta',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_6d6496eb237e4d358670db575dfb1ae7~mv2_d_6000_4000_s_4_2.jpg/v1/fill/w_1260,h_840,al_c,q_85,usm_0.66_1.00_0.01/26357d_6d6496eb237e4d358670db575dfb1ae7~mv2_d_6000_4000_s_4_2.webp'
+    },
+    {
+      id: 10,
+      title: 'RECIPE 10',
+      content: 'mushroom Risotto with Brown & Red Rice ',
+      imgUrl: 'https://static.wixstatic.com/media/26357d_d79c01a32e744205bceece8aed0b309d~mv2_d_6000_4000_s_4_2.jpg/v1/fill/w_1260,h_840,al_c,q_85,usm_0.66_1.00_0.01/26357d_d79c01a32e744205bceece8aed0b309d~mv2_d_6000_4000_s_4_2.webp'
+    },
+  ];
     return (
-      <div style={searchStyle}>
-        <font align="center" size="3" color="green"><h1>Search Results</h1></font>
-      <div className="carouselContainer">
-        <div style={imageStyles}>
-          <button
-            onClick={()=>this.onClick(RIGHT)}
-            className="hollow float-left"
-            style={buttonStyles}>⇦</button>
-          <button
-            onClick={()=>this.onClick(LEFT)}
-            className="hollow float-right"
-            style={buttonStyles}>⇨</button>
-          <div style={{
-            order: 4,
-            justifyContent:'center',
-            alignItems:'center'
-            }}>
-            <button
-              onClick={()=>this.onClick(CENTER)}
-              className="hollow center"
-              style={buttonStyles}>⇪</button>
-            </div>
-          </div>
-        </div>
-        <font align="center" size="1">Recipe: {imageIdx + 1}</font>
-        <font align="center" size="3" color="green"><h1>YOUR SAVED RECIPES</h1></font>
+      <div className="container-hi" style={{
+        width: '90%',
+        margin: 'auto',
+        background: 'white',
+        color: 'grey',
+        minHeight: '100%',
+        minWidth: '100%'
+      }}>
+        <h1 style={{ 'textAlign': 'center' }}>
+          Recipe Search
+        </h1>
+        <CardContainer cards={ cardsData }/>
       </div>
     )
   }
 };
 
 export default SearchRecipe;
-
-
-
 
 
 
