7716609579dd52ac339b23f979096776b4cc876a
Leticia <lzduque@hotmail.com>
2019-06-01 13:58:12 -0400

    ingredients are rendered in alphabetical order

--- food-app/app/controllers/ingredients_controller.rb
+++ food-app/app/controllers/ingredients_controller.rb
@@ -2,7 +2,8 @@ class IngredientsController < ApplicationController
 
   # CREATERECIPE - GET/ - ingredients/ - to render all ingredients in Db --> to render select elements
   def index
-    render json: Ingredient.all
+    render json: Ingredient.order(:name)
+
   end
 
   private
