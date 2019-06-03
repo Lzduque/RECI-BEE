2bc0dbcb99735c449a99d022cac2f6331f0ee511
Sahanah <sahanah.ganesh@gmail.com>
2019-06-03 14:01:48 -0400

    formats api call

--- food-app/client/src/components/MealPlan/MealPlan.jsx
+++ food-app/client/src/components/MealPlan/MealPlan.jsx
@@ -19,7 +19,8 @@ class MealPlan extends Component {
       },
       viewPopup: false,
       viewRecipe: null,
-      viewNutrition: null,
+      servings: null,
+      showNutrition: null,
     }
   };
 
@@ -112,26 +113,75 @@ class MealPlan extends Component {
   }
 
   togglePopup = (state) => {
-    this.setState({viewPopup: state, showPopup: state, viewNutrition: state})
+    this.setState({viewPopup: state, showPopup: state})
+  }
+
+  displayNutrition = (props) => {
+    let result = this.makeRequest(this.transform(this.openNutrition(props)));
+    console.log('result', result);
+    this.setState({
+      showNutrition: result
+    })
   }
 
   openNutrition = (props) => {
-    // console.log('props', props)
-
+    // console.log('props on open nutrition', props)
     var nutrArr = [];
 
     var searchQuantity = (quantities, id) => {
+      // console.log("inside search Quantity");
       return quantities.find(element => element.recipe_id === id).quantity
     }
 
     props.ingredients.map((ingredient) => {
+      // console.log("inside map ingredient for", ingredient);
       nutrArr.push(searchQuantity(ingredient.quantities, props.id));
       nutrArr.push(ingredient.unit);
       nutrArr.push(ingredient.name);
     })
+    this.setState({servings: props.servings});
+    // console.log('serv', props.servings, 'nutrArr', nutrArr);
+    return nutrArr;
+  }
 
-    console.log('Arr', nutrArr);
-    this.setState({viewNutrition: true, viewRecipe: nutrArr})
+  transform = (props) => {
+    let string = [];
+    // console.log('props on transform', props);
+    while (props.length > 0) {
+      let chunk = " " + (props.splice(0, 3).join(" "));
+      string.push(chunk);
+    }
+    var q = string.toString().substring(1);
+    // console.log("finish transform")
+    return q;
+  }
+
+  makeRequest = (param) => {
+    var request = require("request");
+    // console.log("inside make request!")
+    var options = {
+      method: 'POST',
+      url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+      headers: {
+        'postman-token': 'e84dd3d8-9bab-43cc-fbfe-79124b07c6;a3',
+        'cache-control': 'no-cache',
+        accept: 'application/json',
+        'content-type': 'application/json',
+        'x-remote-user-id': '0',
+        'x-app-key': 'aaf88207f36c8f1a7baf0e5f3da7bde7',
+        'x-app-id': 'de975639'
+      },
+      body: {
+        query: param,
+      },
+      json: true
+    };
+
+    request(options, function (error, body) {
+      if (error) throw new Error(error);
+      // console.log('body inside request', body)
+      return body
+    })
   }
 
   render() {
@@ -155,14 +205,15 @@ class MealPlan extends Component {
         <hr />
         <br/>
         <h3>Select Meals for the Day</h3>
-        <br/>
-        <br/>
 
-        {this.state.viewNutrition ? <Nutrition recipe={this.state.viewRecipe}/> : null}
+        { this.state.showNutrition ?
+          <Nutrition servings={this.state.servings} nutrition={this.state.showNutrition}/>
+        : null }
 
         {Object.keys(this.state.choices).map(mealType => {
 
           return (
+
             <div key={mealType} style={{marginBottom: '2rem'}}>
               <h2 style={{'textTransform': 'uppercase', marginBottom: '1rem'}}>{mealType}</h2>
               {
@@ -172,7 +223,7 @@ class MealPlan extends Component {
                     <h4>{this.state.choices[mealType].name}</h4>
                     <img className="chosen-image" onClick={() => this.openView(this.state.choices[mealType])} src={this.state.choices[mealType].image} alt={this.state.choices[mealType].name || 'Image'}/>
                     <button style={buttonStyles} onClick={() => this.filterType(mealType)}>EDIT</button>
-                    <button style={{float: 'right'}} onDoubleClick={() => this.togglePopup(false)} onClick={() => this.openNutrition(this.state.choices[mealType])}>Nutrition</button>
+                    <button style={{float: 'right'}} onClick={() => this.displayNutrition(this.state.choices[mealType])}>Nutrition</button>
                   </div>
                 )
                 :
@@ -194,7 +245,7 @@ class MealPlan extends Component {
             closePopup={() => this.togglePopup(false)}
           />  : null}
 
-      </div>
+       </div>
     )
   }
 };
